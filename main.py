0cf18a1f2cd2d8312c61c90996cd337a301f6525
Ahmet F. Turhan <afaturhan@gmail.com>
2022-10-10 12:44:47 +0300

    accept cookies and click on page

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import selenium as sl
 from selenium import webdriver
 from time import *
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('disable-notifications')
@@ -10,7 +13,17 @@ browser = webdriver.Chrome('chromedriver.exe', options=chrome_options)
 browser.maximize_window()
 
 browser.get('https://www.trendyol.com/sr?q=air force')
-sleep(1)
-browser.find_element("xpath", "//*[@id='onetrust-accept-btn-handler']").click()
-sleep(1)
-browser.find_element("xpath", '//*[@id="search-app"]/div/div[1]/div[2]/div[4]/div[1]/div/div[2]/div[1]/a/div[2]').click()
+delay = 3
+try:    
+    myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located(("xpath", "//*[@id='onetrust-accept-btn-handler']")))
+    print("Cookies Accepted")
+
+    # Click on the cookies button
+    browser.find_element("xpath", "//*[@id='onetrust-accept-btn-handler']").click()
+
+    # Click on the page once
+    browser.find_element("xpath", '//*[@id="search-app"]/div/div[1]/div[2]/div[4]/div[1]/div/div[2]/div[1]/a/div[2]').click() #
+
+except:
+    print("Loading took too much time!")
+    browser.quit()
